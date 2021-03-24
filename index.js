64ee8b3dd552f6aa36f55d6e579edfcd8b44396d
Colin Tseng <klt.aus@yahoo.com>
2021-03-24 21:24:57 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "not James"
+
+
+var upperCaseCustomerName = function() {   
+    b = customerName.toUpperCase();
+    customerName = b
+    return customerName
+}
+
+var setBestCustomer = function() {
+    bestCustomer = "not bob";
+   return bestCustomer
+}
+
+var overwriteBestCustomer = function() {
+    bestCustomer = "maybe bob"
+}
+var changeLeastFavoriteCustomer = function() {
+    leastFavoriteCustomer = "james"
+}
+
+
+
+
+changeLeastFavoriteCustomer();
+upperCaseCustomerName();
+setBestCustomer();
+overwriteBestCustomer();
